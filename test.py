cc981e062b2569785bcba86ec715a6c2a6bd9eda
harshablast <harshablast@gmail.com>
2017-06-17 00:22:47 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,20 +1,34 @@
 import cv2
-import numpy
+import numpy as np
 
-img=cv2.imread('clock2.png',0)
+img=cv2.imread('clock1.png',1)
 
-ret,img_binary=cv2.threshold(img,127,255,0)
+gray_image=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ret,img_binary=cv2.threshold(gray_image,127,255,0)
 img_binary=cv2.bitwise_not(img_binary)
 
-
-
-contours=cv2.findContours(img_binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)[0]
-#ctr=numpy.array(contours).reshape((-1,1,2)).astype(numpy.int32)
-cv2.drawContours(img_binary,contours,-1, (0,255,0), 3)
-cv2.imshow('Original',img)
 cv2.imshow('binary',img_binary)
-print(contours)
+edges=cv2.Canny(img_binary,10,200)
+cv2.imshow('Edges',edges)
+im2, contours, hierarchy = cv2.findContours(edges, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+circles = cv2.HoughCircles(im2,cv2.HOUGH_GRADIENT,1,20,param1=50,param2=30,minRadius=0,maxRadius=0)
 
+circles = np.uint16(np.around(circles))
+maxrad=0
+centre=(0,0)
+for i in circles[0,:]:
+    if i[2]>maxrad:
+        maxrad=i[2]
+        centre=(i[0],i[1])
+
+lines = cv2.HoughLinesP(img_binary,1,np.pi/360,200)
+for i in range(0, len(lines)):
+    for x1,y1,x2,y2 in lines[i]:
+        cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+
+cv2.circle(img,centre,maxrad,(0,255,0),2)
+cv2.drawContours(img, contours, -1, (0,255,0), 1)
+cv2.imshow('final',img)
 
 cv2.waitKey(0)
 
